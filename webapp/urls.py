7896a347b7dd92b919eb67bf356032e3f4d87f9c
mumeinanashii <davisgayamo@gmail.com>
2024-02-02 00:10:06 +0800

    initial commit

--- webapp/urls.py
+++ webapp/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path ('about/', views.about_page, name="aboutpage"),
     path ('reservation/', views.reservation_page, name="reservation"),
     path ('menu/', views.menu_page, name="menu"),
+    path ('update_reservation/<int:pk>/', views.updateReservation, name="updateReservation"),
+    path ('delete_reservation/<int:pk>/', views.deleteReservation, name="deleteReservation")
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -20,6 +20,9 @@ def about_page(request):
     }
     return render(request, 'pages/about.html', context)
 
+def menu_page(request):
+    return render(request, 'pages/menu.html')
+    
 def reservation_page(request):
     reservations = Reservation.objects.all()
     total_reservation = reservations.count()
@@ -37,5 +40,24 @@ def reservation_page(request):
     }
     return render(request, 'pages/reservation.html', context)
 
-def menu_page(request):
-    return render(request, 'pages/menu.html')
+def updateReservation(request, pk):
+    customer = Reservation.objects.get(id=pk)
+    form = reservation(instance=customer)
+
+    if request.method == 'POST':
+        form = reservation(request.POST, instance=customer)
+        if form.is_valid():
+            form.save()
+            return redirect('homepage')
+    context = {'form':form}
+    return render(request, 'pages/reservation.html', context)
+
+
+
+def deleteReservation(request, pk):
+     customer = Reservation.objects.get(id=pk)
+     if request.method == 'POST':
+        customer.delete()
+        return redirect('homepage')
+     context = {'customer':customer}
+     return render(request, 'pages/delete_reservation.html', context)
